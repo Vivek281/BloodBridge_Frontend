ee52035fbdb144a8e513003d69bcd6882e81fdde
Vivek281 <vivekjha2062@gmail.com>
2026-04-09 14:13:45 +0545

    Donation Details Page route fixed

--- src/router/donationRouter.tsx
+++ src/router/donationRouter.tsx
@@ -1,7 +1,8 @@
 import ActiveDonationPage from "../pages/donation/ActiveDonationPage"
+import DonorDetailsPage from "../pages/donation/DonorDetailsPage"
 
 export const donationRouter = [
     {path:"/donation-active/:id", element:<ActiveDonationPage/>},
-    {path:"/donor/:id", element:<ActiveDonationPage/>},
+    {path:"/donor/:id", element:<DonorDetailsPage/>},
 
 ]
